7858768daafa0e47f3c78c2ce57c5d6713ba4516
Connor Turland <connorturland@gmail.com>
2019-02-13 09:32:25 -0500

    wip

---
+++ events-creator-notes.js
@@ -0,0 +1,35 @@
+
+
+start with building and showing a user interface, no backend, just open it in a web browser
+
+
+show how you would use Javascript to call Holochain functions
+
+
+show how to install the command line tools
+
+
+show to run a Development conductor
+
+
+Connect the user interface to the backend
+
+
+Adding the Zome function for your UI to call
+
+
+
+
+
+
+
+
+Events app
+- post an event
+- browse and search events
+- respond to whether you're attending an event
+- message the event organizer
+
+
+- get some designer time for the UI
+
